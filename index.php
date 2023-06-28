25614f8fa97cf7536a78701d7bf429603c1909d9
MaranoMMV <matheusmmv14@gmail.com>
2023-06-28 18:16:53 -0300

    Atualizacao

--- index.php
+++ index.php
@@ -8,23 +8,44 @@ include_once("templates/menu.php");
 
                 <h2>Curriculo</h2>
                 <br>
-                <h3>Contact</h3>
-                <p><b>tell: </b>(11) 96838-1616</p>
-                <p><b>Email: </b> matheus.marano@outlook.com</p>
-                <p><b>Website:</b> <a href="www.maranoart.com.br">www.maranoart.com.br</a>
-                <br>
+                <h3>Formação Educacional:</h3>
+                <p>[Local: Brasil] Ensino Médio Completo - Escola Estadual Professor Miguel Sansigolo – Concluído em dezembro/2019.</p>
+                <p>[Local: Brasil] Ensino Superior - Faculdade Das Américas. Sistemas de informação 6ºSemestre – Previsão de conclusão dezembro/2023.<p>
                 <br>
                 <h3>Certificates</h3>
-                <p>[Local: Brasil] Curso de Qualificação profissional – City Computer – Concluído em abril 2019.</p>
-                <p>[Local: Brasil] Curso de desenvolvimento de sites - Etec Vila Formosa - Concluído em dezembro de 2019.</p>
-                <p>[Local: Brasil] Curso de Excel do básico – Senac Vila Prudente – Concluído em dezembro de 2021.</p>
-                <p>[Local: Brasil] Curso de Programação PHP do zero a Maestria + 4 Projetos incríveis - Udemy - concluído em Setembro de 2022.</p>
-                <p>[Local: Brasil] Cursando Inglês – Friends English School – Previsão de conclusão em dezembro de 2024.</p>
-                <p>[Locale: Brazil] course Java COMPLETO 2023 Programação Orientada a Objetos +Projetos - complected 27/06/2023</p>
+                <p>Curso de Qualificação profissional – City Computer – Concluído em abril 2019.</p>
+                <p>Curso de desenvolvimento de sites - Etec Vila Formosa - Concluído em dezembro de 2019.</p>
+                <p>Curso de Excel do básico – Senac Vila Prudente – Concluído em dezembro de 2021.</p>
+                <p>Curso de Programação PHP do zero a Maestria + 4 Projetos incríveis - Udemy - concluído em Setembro de 2022.</p>
+                <p>Course Java COMPLETO 2023 Programação Orientada a Objetos + Projetos - complected 27/06/2023</p>
+                <p>Cursando Inglês – Friends English School – Previsão de conclusão em dezembro de 2024.</p>
                 <br>
                 <h3> Languages </h3>
                 <p>Português: Nativo </p>
                 <p>Inglês: Intermediario</p>
                 <br>
                 <h3> Tools and systems </h3>
+                <p>Exel: Avançado.</p>
+                <p>Pacote office: Intermediário.</p>
+                <p>PowerBi: Intermediário.</p>
+                <p>Photoshop: Básico.</p>
+                <p>Dreamweaver: Básico.</p>
+                <p>Active Directory: Intermediário.</p>
+                <p>Visual Studio Code: Básico.</p>
+                <p>Hyper-V e VirtualBox: Intermediário.</p>
+                <p>Github: Básico.</p>
+                <p>SQL: Intermediário.</p>
+                <p>Sistema SOC: Intermediário.</p>
+                <p>Visio: Básico.</p>
+                <p>Windows: Avançado.</p>
+                <p>Windows Server: Básico.</p>
+                <p>MacOS: Básico.</p>
+                <br>
+                <h3> Linguagens de programação:</h3>
+                <p>Java: Intermediário</p>
+                <p>HTML: Intermediário.</p>
+                <p>CSS: Intermediário.</p>
+                <p>PHP: Intermeriário.</p>
+                <p>JavaScript: Básico.</p>
+                <br>
             </section>
